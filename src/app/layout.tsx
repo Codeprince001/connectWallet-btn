5197e1dede5629ad428d2cab430c0f83265cf8f6
Codeprince001 <urangyorkwisdom@gmail.com>
2024-09-14 22:52:41 +0100

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import AppWalletProvider from "./components/AppWalletProvider";
+import AppWalletProvider from "@/components/AppWalletProvider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
