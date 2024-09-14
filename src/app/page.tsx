5f7cb826234ab40f3be1e1bcc863327bf88556f6
Codeprince001 <urangyorkwisdom@gmail.com>
2024-09-14 15:34:39 +0100

    removed gradientbg import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,32 @@
 "use client";
  
-import PublicBtn from "@/components/MitingBtn/PublicBtn";
-import WhitielistBtn from "@/components/MitingBtn/WhitielistBtn";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import Image from "next/image";
 import NFT1 from "../assets/Monking.png"
+import { useState } from "react";
+import WhitielistBtn from "@/components/ActionsBtn/WhitielistBtn";
+import PublicBtn from "@/components/ActionsBtn/PublicBtn";
 
- 
+
+
 export default function Home() {
+
+  const [isPublic, setisPublic] = useState()
+
   return (
     <main className="min-h-screen">
-      <div className="flex flex-1 justify-between m-20">
-          <h1 className="text-white text-3xl font-extrabold">NFT MINT</h1>
-                <div className="border hover:border-slate-900 rounded">
-                  <WalletMultiButton style={{}} />
+      <div className="flex flex-1 justify-between m-10">
+          <h1 className="text-white text-3xl font-extrabold">ERROR 404</h1>
+                <div className="border rounded">
+                  <WalletMultiButton style={{backgroundColor: 'black', border: 'none'}} />
               </div>
         </div>
 
       
-      <div className="mx-[15rem] my-[5rem] border-2 rounded-md p-6">
+      <div className="mx-[10rem] my-[5rem] border-1 border-[#ccc] rounded-md p-2">
         <div className="flex justify-between gap-10 mt-4">
           <div>
-            <p className="max-w-[60%]">
+            <p className="md:max-w-[70%] max-w-[60%] md:text-[1.3rem]">
               Welcome to the future of digital ownership with our Web3 NFT Minting Platform,
               where creativity meets blockchain technology.
             </p>
