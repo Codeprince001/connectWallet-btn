27003b55b18693c20ba5146fbd3ad946d70de082
Codeprince001 <urangyorkwisdom@gmail.com>
2024-09-14 15:35:17 +0100

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import AppWalletProvider from "./components/AppWalletProvider";
-import Gradientbg from "../assets/dark-bg.jpg"
 
 export const metadata: Metadata = {
   title: "Create Next App",
