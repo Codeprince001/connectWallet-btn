706608e74fc3b9e76fe72fc5b25d3ed013c5288f
Codeprince001 <urangyorkwisdom@gmail.com>
2024-09-14 22:53:41 +0100

    feat: added onclick logic for buttons

--- src/components/ActionsBtn/PublicBtn.tsx
+++ src/components/ActionsBtn/PublicBtn.tsx
@@ -3,15 +3,16 @@ import Link from 'next/link';
 
 
 
-const PublicBtn = () => {
+const PublicBtn = ({onClick}: {onClick: () => void}) => {
   return (
-    <div className='bg-[#1d1f28] p-5 rounded-lg hover:bg-[#111230]'>
-    <Link href="/">
+    <div 
+      className='bg-[#1d1f28] p-5 rounded-lg hover:bg-[#111230] hover:cursor-pointer'
+      onClick={onClick}
+    >
       <div>
         <p className='text-xl font-bold'>Public </p>
         <p className='text-[#767373]'> Supply: {0}</p>
       </div>
-    </Link>
   </div>
   )
 }
--- src/components/ActionsBtn/WhitielistBtn.tsx
+++ src/components/ActionsBtn/WhitielistBtn.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Link from 'next/link'
 
-const WhitielistBtn = () => {
+const WhitielistBtn = ({onClick}: {onClick: () => void}) => {
   return (
-    <div className='bg-[#1d1f28] p-5 rounded-lg hover:bg-[#111230]'>
-      <Link href="/">
+    <div 
+      className='bg-[#1d1f28] p-5 rounded-lg hover:bg-[#111230] hover:cursor-pointer'
+      onClick={onClick}
+    >
         <div>
           <p className='text-xl font-bold'>Whitelist </p>
           <p className='text-[#767373]'> Supply: {0}</p>
         </div>
-      </Link>
     </div>
   )
 }
