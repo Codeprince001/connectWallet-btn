14a78f52b638d3e28724c9cdba65e1ae62adc6e6
Codeprince001 <urangyorkwisdom@gmail.com>
2024-09-19 06:11:45 +0100

    layou/page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,61 @@
 "use client";
  
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import Image from "next/image";
-import NFT1 from "../assets/Monking.png"
+
 import { useState } from "react";
-import WhitielistBtn from "@/components/ActionsBtn/WhitielistBtn";
-import PublicBtn from "@/components/ActionsBtn/PublicBtn";
+import WhitielistBtn from "@/components/MintingButton/WhitielistBtn";
+import PublicBtn from "@/components/MintingButton/PublicBtn";
+
+import Socials from "@/components/Socials";
+import MintingCard from "@/components/MintingCard";
 import MintButton from "@/components/MintButton";
+import Header from "@/components/Header";
 
 
 
 export default function Home() {
 
   const [mintOption, setMintOption] = useState<string>("")
+  const [activeButton, setActiveButton] = useState<string>("whitelist")
+  console.log(activeButton)
 
   const handleMintOptionChange = (mintOption: string) => {
     setMintOption(mintOption)
-    console.log(mintOption)
+    setActiveButton(mintOption)
   }
 
+
   return (
-    <main className="min-h-screen">
-      <div className="flex flex-1 justify-between bg-[#0502366f] p-4">
-          <h1 className="text-white text-3xl font-extrabold">ERROR 404</h1>
-                <div className="border rounded">
-                  <WalletMultiButton style={{backgroundColor: 'black', border: 'none'}} />
-              </div>
-        </div>
+    <main className="min-h-screen bg-[url('../assets/404.png')] bg-cover relative" style={{ height: "100vh", width: "100vw"}}>
+        <div className="absolute inset-0 bg-gradient-to-b from-[#000]/80 to-[#000]/70">
 
+         <Header/>
       
-      <div className="mx-[10rem] my-[5rem] border-1 border-[#ccc] rounded-md p-2">
-
-        <div className="flex justify-between gap-10 mt-4">
-          <div>
-            <p className="md:max-w-[70%] max-w-[60%] md:text-[1.3rem]">
-              Welcome to the future of digital ownership with our Web3 NFT Minting Platform,
-              where creativity meets blockchain technology.
+      <div className="mx-[10rem] md:mt-[2rem] px-4 py-2 bg-[#050113] rounded-4xl">
+        <div className="flex justify-between gap-[2rem] mt-1 items-center">
+          <div className="p-4">
+            <p className="md:max-w-[80%] max-w-[60%] md:text-[1.3rem]">
+              Error 404 is an Exclusive NFT Project on Solana, featuring a limited supply of 404 unique NFTs in the OG collections.
             </p>
 
             {/* Action button details and NFT Image */}
             <div className="flex flex-col gap-4 mt-6 flex-1">
-              <WhitielistBtn onClick={() => handleMintOptionChange("Whitelist")}/>
-              <PublicBtn onClick={() => handleMintOptionChange("public")}/>
+              <WhitielistBtn
+                onClick={() => handleMintOptionChange("whitelist")}
+                isActive={activeButton === "whitelist"}
+              />
+              <PublicBtn 
+                onClick={() => handleMintOptionChange("public")}
+                isActive={activeButton === "public"}
+              />
             </div>
           </div>
 
-          <div className="h-[100%]">
-            <Image
-              className="h-[100%]"
-              src={NFT1}
-              alt="Nft image"
-            />
+          <div className="md:max-w-[50%]">
+           <MintingCard/>
           </div>
         </div>
-
-
-        <div className="flex flex-1 justify-end mt-[3rem]">
-          {
-            mintOption === "public"
-              ? 
-            (<MintButton mintOption="Mint public"/>)
-              : 
-            (<MintButton mintOption="Mint whitelist"/>)
-          }
-        </div>
+      </div>
       </div>
     </main>
   );
